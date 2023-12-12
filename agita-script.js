075a98e64523f132e592ee9082f9c45b1906167a
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-12-12 21:00:29 +0700

    menambahkan soal

--- agita-script.js
+++ agita-script.js
@@ -1,3 +1,4 @@
+//1. Buatlah fungsi dengan parameter berapa banyak angka yg akan ditampilkan secara looping, dimana jika angka tersebut habis dibagi 3 cetak "fish" jika habis dibagi 5 cetak "bash" dan jika habis dibagi 15 cetak "fish bash", cetak cukup salah satu dari kondisi tersebut, cetak menggunakan console log atau sejenisnya.
 function fishBash(arr = []) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 3 === 0) {
@@ -11,6 +12,8 @@ function fishBash(arr = []) {
     }
   }
 }
+
+//2. Buatlah fungsi dengan input array integer untuk mengurutkan angka dari kecil kebesar dan besar kekecil dengan manual tanpa fungsi bawaan javascript,
 
 function sort(arr = [], type = "asc") {
   var input = arr;
@@ -37,6 +40,8 @@ function sort(arr = [], type = "asc") {
   }
   return output;
 }
+
+//3. Buatlah fungsi dengan input string jika string tersebut dibalik dan tetap sama dengan string yang diinputkan maka return true jika tidak return false
 
 function reverseString(string = "") {
   const reverseWord = string.split("").reverse().join("");
